e20ecebf3708f686599cbb2cf1bdadd4646e994b
Leon <leon@tropos.de>
2025-11-03 09:49:26 +0100

    Added SST to time series plots

--- plots.py
+++ plots.py
@@ -8,12 +8,14 @@ import matplotlib.path as mpath
 import matplotlib.colors as mcolors
 from matplotlib.ticker import FormatStrFormatter
 import pymannkendall as mk
+from scipy.stats import alpha
 from statsmodels.tsa.stattools import acf
 import statsmodels.api as sm
 from cartopy.mpl.gridliner import LATITUDE_FORMATTER
 from matplotlib import ticker as mticker
 import global_vars
 from matplotlib.colors import LinearSegmentedColormap
+from scipy import stats
 
 def truncate_colormap(cmap, minval=0.0, maxval=0.5, n=256):
     new_colors = cmap(np.linspace(minval, maxval, n))
@@ -25,8 +27,23 @@ def format_func(value, tick_number):
     N = int(value + 1990)
     return N
 
-def plot_fit(ax, t_ax, p_fit, eq, color, a):
-    return ax.plot(t_ax, p_fit, color, linestyle='dashed', label=eq, linewidth=1, alpha=a)
+def get_lin_fit(sl, itc, C):
+    p_fit = [p * sl + itc for p in C]
+    eq = f'{sl[0]:.1e}x + {itc[0]:.1e}'
+    return p_fit, eq
+
+def plot_fit(ax, t_ax, p_fit, eq, color, a, s):
+    return ax.plot(t_ax, p_fit, color, linestyle=s, label=eq, linewidth=1.7, alpha=a)
+
+def get_lin_regression(C, variables):
+    sl, itc, signf, rval = [], [], [], []
+    for var in variables:
+        model = stats.linregress(C, var)
+        sl.append(model.slope)
+        itc.append(model.intercept)
+        signf.append(model.pvalue)
+        rval.append(round(model.rvalue, 1))
+    return sl, itc, signf, rval
 
 def create_histogram(C, title, var_type):
     y = C[1]['1990-2019']['data_aver_reg'].values
@@ -66,59 +83,67 @@ def autocorrelation(C, title, var_type):
     plt.close()
 
 def plot_fit_trends(ax, C, title, axis_label, vm, colors, leg, fig_name, var_type,
-                    echam_data=True, seaice=False, multipanel=False):
+                    echam_data=True, seaice=False, multipanel=False,
+                    thesis_plot=False):
     """ Plots yearly data of sea ice area and emission anomalies and calculates the 30-year trend
     :returns subplot objects to later add the legend """
+    f = 15
     if multipanel:
         pass
     else:
         fig, ax = plt.subplots(1, 1,
                                figsize=(8, 3), )
 
-    C_ice, C_biom = C[0]['1990-2019']['data_sum_reg'], C[1]['1990-2019']['data_aver_reg']
+    C_ice = C[0]['1990-2019']['data_sum_reg']
+    C_emi = C[1]['1990-2019']['data_aver_reg']
+    C_sst = C[2]['1990-2019']['data_aver_reg']
+
+    print('MEAN VALS',
+          np.mean(C[2]['1990-2004']['data_aver_reg']),
+          np.mean(C[2]['2005-2019']['data_aver_reg']))
+
     t_ax = C_ice.time.values
 
     ax2 = ax.twinx()
+
     new_line_color = colors[0]
     if seaice:
         new_line_color = 'lightblue'
         ax.fill_between(t_ax, 0, C_ice, alpha=0.2, color=new_line_color)
-        p1, = ax.plot(t_ax, C_ice, new_line_color, label=leg[0], linewidth=1.)
-    ax.scatter(t_ax, C_ice, s=7, c=new_line_color)
+        p1, = ax.plot(t_ax, C_ice, new_line_color, label=leg[0], linewidth=1.7)
+    ax.scatter(t_ax, C_ice, s=10, c=new_line_color)
 
-    p2, = ax2.plot(t_ax, C_biom, colors[1], label=leg[1], linewidth=1.)
-    ax2.scatter(t_ax, C_biom, s=7, c=colors[1])
-    ax2.axhline(y=0.0, c=colors[1], linewidth=0.5, alpha=0.2)
+    p2, = ax2.plot(t_ax, C_emi, colors[1], label=leg[1], linewidth=1.7)
+    ax2.scatter(t_ax, C_emi, s=10, c=colors[1])
+    ax2.axhline(y=0.0, c=colors[1], linewidth=0.5, alpha=0.5)
 
-    decades = ['1990-1999', '2000-2009', '2010-2019']
-    decades = ['1990-2004', '2005-2019']
+    # decades = ['1990-1999', '2000-2009', '2010-2019']
+    # decades = ['1990-2004', '2005-2019']
 
     a = [0.5, 1]
-    f1_list, f2_list = [], []
+    f1_list, f2_list, f3_list = [], [], []
     # for idx, dec in enumerate(decades):
     if global_vars.season_to_analise == 'JAS' or global_vars.season_to_analise == 'AMJ':
         dec = '1990-2019'
         idx = 1
         t_ax = C[0][dec]['data_sum_reg'].time.values
 
-        C_ice = C[0][dec]['data_sum_reg'].values
         model_sic = mk.original_test(C_ice)
-
-        C_emi = C[1][dec]['data_aver_reg'].values
         model_emi = mk.original_test(C_emi)
+        model_sst = mk.original_test(C_sst)
 
-        sl = [model_sic.slope, model_emi.slope]
-        itc = [model_sic.intercept, model_emi.intercept]
-        significance = [model_sic.h, model_emi.h]
-        trend = [model_sic.trend, model_emi.trend]
+        sl = [model_sic.slope, model_emi.slope, model_sst.slope]
+        itc = [model_sic.intercept, model_emi.intercept, model_sst.intercept]
+        significance = [model_sic.h, model_emi.h, model_sst.h]
+        trend = [model_sic.trend, model_emi.trend, model_sst.trend]
 
-        print('SIC TAU',C_ice, model_sic.Tau, '\n',min(C_ice), max(C_ice), '\n')
-        print('Emission TAU', C_emi, model_emi.Tau, '\n',min(C_emi), max(C_emi), '\n\n')
+        # print('SIC TAU',C_ice, model_sic.Tau, '\n',min(C_ice), max(C_ice), '\n')
+        # print('Emission TAU', C_emi, model_emi.Tau, '\n',min(C_emi), max(C_emi), '\n\n')
 
         p_fit = [p * sl[0] + itc[0] for p in np.arange(len(t_ax))]
         eq = f'{sl[0]:.1e}x + {itc[0]:.1e}'
         if significance[0]:
-            f1, = plot_fit(ax, t_ax, p_fit, eq, colors[0], a[idx])
+            f1, = plot_fit(ax, t_ax, p_fit, eq, colors[0], a[idx], 'dashed')
             f1_list.append(f1)
         else:
             f1_list.append(p1)
@@ -126,36 +151,64 @@ def plot_fit_trends(ax, C, title, axis_label, vm, colors, leg, fig_name, var_typ
         p_fit = [p * sl[1] + itc[1] for p in np.arange(len(t_ax))]
         eq = f'{sl[1]:.1e}x + {itc[1]:.1e}'
         if significance[1]:
-            f2, = plot_fit(ax2, t_ax, p_fit, eq, colors[1], a[idx])
+            f2, = plot_fit(ax2, t_ax, p_fit, eq, colors[1], a[idx], 'dashed')
             f2_list.append(f2)
         else:
             f2_list.append(p2)
 
-    ax.set_ylabel(axis_label[0], fontsize=8)
+        if not thesis_plot:
+            ax3 = ax.twinx()
+            ax3.spines.right.set_position(("axes", 1.4))
+            p3, = ax3.plot(t_ax, C_sst, colors[2], label=leg[2], linewidth=1.)
+            ax3.scatter(t_ax, C_sst, s=7, c=colors[2])
+            p_fit = [p * sl[2] + itc[2] for p in np.arange(len(t_ax))]
+            eq = f'{sl[2]:.1e}x + {itc[2]:.1e}'
+            if significance[2]:
+                f3, = plot_fit(ax3, t_ax, p_fit, eq, colors[2], a[idx], 'dashed')
+                f3_list.append(f3)
+            else:
+                f3_list.append(p3)
+            ax_list = [ax2, ax3]
+            ax.xaxis.set_tick_params(labelsize=f)
+            if echam_data:
+                ax.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
+        else:
+            ax_list = [ax2]
+            ax.tick_params(axis='x', labelbottom=False)
+        # print('\n', 'MIN and MAX SST', np.min(C_sst), C_sst.argmin(),
+        #       np.max(C_sst), C_sst.argmax(), '\n')
+        print('\n', 'MIN and MAX SIC', np.min(C_ice), C_ice.argmin(),
+              C_ice,
+              np.max(C_ice), C_ice.argmax(), '\n')
+
+
+    ax.set_ylabel(axis_label[0], fontsize=f)
     ax.tick_params(axis='x')
 
-    ax.yaxis.get_label().set_fontsize(8)
-    ax2.set_ylabel(axis_label[1], fontsize=8)
-    ax.yaxis.set_tick_params(labelsize=8)
-    ax2.yaxis.set_tick_params(labelsize=8, color=colors[1])
-    ax2.tick_params(axis='y', colors=colors[1])
-    ax2.yaxis.label.set_color(colors[1])
+    ax.yaxis.get_label().set_fontsize(f)
+    ax.yaxis.set_tick_params(labelsize=f)
+
+
+    for idx, a in enumerate(ax_list):
+        a.set_ylabel(axis_label[idx+1], fontsize=f)
+        a.yaxis.set_tick_params(labelsize=f, color=colors[idx+1])
+        a.tick_params(axis='y', colors=colors[idx+1])
+        a.yaxis.label.set_color(colors[idx+1])
 
     if title[0] == 'Arctic':
         tt = title[0] + '\n \n \n'
     else:
         tt = '\n '+ title[0] + '\n \n '
 
-    ax.set_title(tt, loc='center', fontsize=10)
-    ax.set_title(title[1], loc='left', fontsize=10)
+    ax.set_title(tt, loc='center', fontsize=f+2)
+    ax.set_title(title[1], loc='left', fontsize=f+2)
 
 
     ax.set_ylim(vm[0][0], None)
     ax2.set_ylim(vm[1][0], vm[1][1])
 
     #ax.grid(linestyle='--', linewidth=0.4)
-    ax.xaxis.set_tick_params(labelsize=8)
-    ax.yaxis.set_tick_params(labelsize=8, color=colors[0])
+    ax.yaxis.set_tick_params(labelsize=f, color=colors[0])
     ax.tick_params(axis='y', colors=colors[0])
     ax.yaxis.label.set_color(colors[0])
 
@@ -163,9 +216,7 @@ def plot_fit_trends(ax, C, title, axis_label, vm, colors, leg, fig_name, var_typ
 
     ax.yaxis.set_major_formatter(FormatStrFormatter('%.3g'))
     ax2.yaxis.set_major_formatter(FormatStrFormatter('%.3g'))
-
-    if echam_data:
-        ax.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
+    ax.grid(linestyle='--', linewidth=0.3)
 
     if multipanel:
         pass
--- time_series.py
+++ time_series.py
@@ -5,6 +5,7 @@ import global_vars
 import plots, utils
 import matplotlib.pyplot as plt
 
+from global_vars import thesis_plot
 from utils import regions
 
 if __name__ == '__main__':
@@ -14,12 +15,41 @@ if __name__ == '__main__':
         variables_info_yr = pickle.load(myFile)
 
     unit = '(Tg season${^{-1}}$)'
-    fig, axes = plt.subplots(3, 1,
-                             figsize=(6, 7), )
-    axs = axes.flatten()
-    colors = ['darkblue', 'darkred']
+    if global_vars.thesis_plot:
+        fig, axes = plt.subplots(3, 1,
+                                 figsize=(6, 8), )
+        axs = axes.flatten()
+
+    else:
+
+        plot_heights = [2, 1, 2, 1, 2,1]
+        height_ratios = [
+            2.2, 0.01,
+            1.3, 0.1,
+            2.2, 0.01,
+            1.3, 0.1,
+            2.2, 0.01,
+            1.3,
+        ]
+        fig, axes = plt.subplots(len(height_ratios), 1,
+                                 figsize=(10, 16),
+                                 gridspec_kw={'height_ratios': height_ratios, 'hspace': 0.1},
+                                 constrained_layout = True,
+        )
+        ax = axes.flatten()
+        for i, a in enumerate(ax):
+            if i == 1 or i == 3 or i == 5 or i == 7 or i == 9:
+                a.axis('off')
+        axs = [ax[0], ax[4], ax[8]]
+        axs1 = [ax[2], ax[6], ax[10]]
+    colors = ['darkblue', 'darkred', 'gray']
 
     seaice = utils.get_seaice_vals(variables_info_yr, 'Sea_ice')
+    var = 'AER_SST' #'AER_SIC_area_px'
+    sst = variables_info_yr['AER_SST']
+    omf = variables_info_yr['OMF_tot']
+    u10 = variables_info_yr['AER_U10']
+
     seaice_lin = variables_info_yr['AER_SIC_area_px']
     var_type = ['Biom_tot', 'biom', 'Biomolecule concentration']
     var_type = ['AER_tot', 'aer_conc', 'Aerosol concentration']
@@ -29,7 +59,7 @@ if __name__ == '__main__':
     flux = variables_info_yr[var_type[0]]  # AER_LIP #AER_F_tot_yr
     region = {'Arctic':{'lims':[[4.3, 7.5], [-0.7e-5, 0.7e-5]],
                         'label': r'$\bf{(a)}$'},
-            'Beaufort Sea':{'lims':[[0., 0.6], [-0.7e-5, 0.7e-5]],
+            'Beaufort Sea':{'lims':[[0.45, 0.6], [-0.7e-5, 0.7e-5]],
                         'label': r'$\bf{(b)}$'},
             'Barents Sea':{'lims':[[0, 0.23], [-2.5e-5, 2.5e-5]],
                         'label': r'$\bf{(c)}$'}}
@@ -47,26 +77,71 @@ if __name__ == '__main__':
                         [reg, r'$\bf{(a)}$'],
                          var_type)
         leg = plots.plot_fit_trends(axs[idx],
-                                    [seaice_lin[reg], flux[reg]],
-                                    [reg, region[reg]['label']],
-                                    ['Sea Ice Area \n (millions of km$^{2}$)', f'PMOA emission \n anomalies {unit}'],
-                                    # ['Sea ice \n Concentration ($million km^{2})$', 'Total biomolecule \n concentration'],
-                                    # [[6, 10.5], [0.1, 0.1]],
-                                    region[reg]['lims'],
-                                    colors,
-                                    ['Sea ice', 'PMOA'],
-                                    # ['Sea Ice', 'Biomolecules'],
-                                    f'{season}_Ocean_Flux_PL',
-                                    var_type,
-                                    echam_data=True,
-                                    seaice=True,
-                                    multipanel=True)
+                            [seaice_lin[reg], flux[reg], sst[reg]],
+                            [reg, region[reg]['label']],
+                            [f'Sea Ice Area \n (10$^{6}$ km$^{2}$)',
+                             f'PMOA emission \n anomalies {unit}', 'SST ($^{o}$C)'],
+                            # ['Sea ice \n Concentration ($million km^{2})$', 'Total biomolecule \n concentration'],
+                            # [[6, 10.5], [0.1, 0.1]],
+                            region[reg]['lims'],
+                            colors,
+                            ['Sea ice', 'PMOA', 'SST'],
+                            # ['Sea Ice', 'Biomolecules'],
+                            f'{season}_Ocean_Flux_PL',
+                            var_type,
+                            echam_data=True,
+                            seaice=True,
+                            multipanel=True,
+                            thesis_plot=True)
         if idx == 0:
             plt.legend(handles=[leg[0], leg[2][0], leg[1], leg[3][0]], ncol=2,
-                       bbox_to_anchor=(0.1, 1.), loc='lower left', fontsize=8)
+                       bbox_to_anchor=(0.1, 1.), loc='lower left', fontsize=15)
         else:
             plt.legend(handles=[leg[2][0],  leg[3][0]], ncol=2,  # ncol=len(axs[0].lines),
-                       bbox_to_anchor=(0.1, 1.), loc='lower left', fontsize=8)
+                       bbox_to_anchor=(0.1, 1.), loc='lower left', fontsize=15)
+
+        if not thesis_plot:
+            leg = plots.plot_fit_trends_sst(axs1[idx],
+                                        sst[reg],
+                                        [reg, region[reg]['label']],
+                                        'SST ($^{o}$C)',
+                                        'gray')
+
+    # plt.tight_layout()
+    plt.savefig(f'./plots/{season}_multipanel_time_series.png',
+                dpi=300)
+    plt.close()
+
+    fig2, axes2 = plt.subplots(3, 1,
+                             figsize=(7, 10), )
+    axs2 = axes2.flatten()
+    # region = {'Kara Sea':{'lims':[[4.3, 7.5], [-0.7e-5, 0.7e-5]],
+    #                     'label': r'$\bf{(a)}$'},
+    #         'Laptev Sea':{'lims':[[0., 0.6], [-0.7e-5, 0.7e-5]],
+    #                     'label': r'$\bf{(b)}$'},
+    #         'Chukchi Sea':{'lims':[[0, 0.23], [-2.5e-5, 2.5e-5]],
+    #                     'label': r'$\bf{(c)}$'}}
+
+
+
+    sst = variables_info_yr['AER_SST']
+    omf = variables_info_yr['OMF_tot']
+    u10 = variables_info_yr['AER_U10']
+    seaice_lin = variables_info_yr['AER_SIC']
+    var_type = ['AER_F_tot', 'aer_flux', 'Aerosol emission flux']
+
+    names = ['SIC', 'SST', 'u10', 'OMF']
+    for idx, reg in enumerate(list(utils.regions().keys())):
+        variables = []
+        variables.append(100-seaice_lin[reg]['1990-2019']['data_aver_reg'].values)
+        for i in [sst, u10, omf, flux]:
+            variables.append(i[reg]['1990-2019']['data_aver_reg'].values)
+        sl, itc, significance, rval = plots.get_lin_regression(variables[-1], variables[:-1],)
+        print('\n', reg, ' correlation with ', var_type[-1])
+        for sdx, s in enumerate(significance):
+            if s < 0.05:
+                n = f'r = {rval[sdx]}'
+                print(names[sdx], n)
 
     plt.tight_layout()
     plt.savefig(f'./plots/{season}_multipanel_time_series.png', dpi=300)
